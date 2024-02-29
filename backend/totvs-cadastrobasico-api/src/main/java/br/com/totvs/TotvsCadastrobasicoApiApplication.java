cfdd76b4c9940c7a59764da8f2c16faefb6a94dd
Januario <januario86fp@gmail.com>
2024-02-29 11:51:55 -0300

    estrutura inicial backend para construção das apis

---
+++ backend/totvs-cadastrobasico-api/src/main/java/br/com/totvs/TotvsCadastrobasicoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.totvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotvsCadastrobasicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotvsCadastrobasicoApiApplication.class, args);
+	}
+
+}
